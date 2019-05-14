71d1e68a01557cfb89976bdde0f311f371033805
aluno <firion_@msn.com>
2019-05-13 21:20:16 -0300

    Teste Vue

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <div id="app">
+
+<Teste></Teste>
+
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+import Teste from './components/teste.vue'
+
+export default {
+  name: 'app',
+  components: {
+    HelloWorld,
+    Teste
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/teste.vue
@@ -0,0 +1,25 @@
+<template>
+    <div>
+        <p>Isso é um teste de componente!!</p>
+
+        <p>Voce digitou: {{valor}} </p>
+
+        <input type="text=" v-model="valor">
+
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "teste",
+        data: function () {
+            return {
+                valor:""
+            }
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
